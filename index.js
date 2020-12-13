c1da65a4827245dc3c0b01953c5998171b7325d4
yatin mahajan <42390921+yatin428@users.noreply.github.com>
2020-12-13 21:26:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors'; 
 
-import App from './App';
+import postRoutes from './routes/posts.js';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const app = express();
+
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://anmysto:enormouslibra42@cluster0.imgtl.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+   .then(() => app.listen(PORT,() => console.log(`Server running on port: ${PORT}`)))
+   .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify' , false);
