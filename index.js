dab148df052f66c5391c2dc80a9cfe74013a36f0
yatin mahajan <42390921+yatin428@users.noreply.github.com>
2021-02-15 01:51:57 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import App from './App';
+import postRoutes from './routes/posts.js';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const app=express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: '30mb',extended:true}));
+app.use(bodyParser.urlencoded({limit: '30mb',extended:true}));
+app.use(cors());
+
+
+app.use('/posts', postRoutes);
+
+app.get('/',(req,res) => {
+res.send('Hello to memories API');
+});
+    
+
+//const CONNECTION_URL = 'mongodb+srv://anmysto:enormouslibra42@cluster0.qxxz2.mongodb.net/Anmysto?retryWrites=true&w=majority'
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true,useUnifiedTopology: true})
+ .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+ .catch((error) => console.log(error.message));
+
+
+ mongoose.set('useFindAndModify',false);
